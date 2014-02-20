839b3ef3244352bc1e04ade5003608be069a122a
Robert Ferney <capnregex@gmail.com>
2014-02-20 16:55:46 -0700

    cleaning up article views

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,7 +5,9 @@ class Ability
     user ||= User.new # guest user (not logged in)
 
     if user.has_role? :admin
+        can :manage, :all
         can :invite, User
+        can :manage, Article
     end
 
     # Define abilities for the passed in user here. For example:
