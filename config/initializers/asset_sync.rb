561587424a3fa40a490b4f5a5a503e17bb5ac076
Robert Ferney <capnregex@gmail.com>
2014-02-21 01:25:08 -0700

    asset_sync config

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -29,7 +29,7 @@ AssetSync.configure do |config|
 end
 
 Fog.credentials = { 
-  endpoint: 'https://s3-website-us-west-2.amazonaws.com/',
+  endpoint: 'https://s3-us-west-2.amazonaws.com/',
   path_style: true 
 }
 
