799ac9e8d981378bdb6a64f3861021218a932157
keith ferney <keith@ferney.org>
2014-06-19 14:10:22 -0600

    fixed #29 add last names

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140521220402) do
+ActiveRecord::Schema.define(version: 20140619194146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -104,6 +104,7 @@ ActiveRecord::Schema.define(version: 20140521220402) do
     t.string   "portrait_content_type"
     t.integer  "portrait_file_size"
     t.datetime "portrait_updated_at"
+    t.string   "last"
   end
 
   add_index "people", ["family_id"], name: "index_people_on_family_id", using: :btree
